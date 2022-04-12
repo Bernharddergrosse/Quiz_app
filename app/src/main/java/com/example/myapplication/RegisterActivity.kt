4315c00246200da960354ea3cf7c8dbc3ad21430
Bernharddergrosse <61414126+Bernharddergrosse@users.noreply.github.com>
2022-04-12 14:25:11 +0200

    Add Register functionality

---
+++ app/src/main/java/com/example/myapplication/RegisterActivity.kt
@@ -0,0 +1,63 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.RegisterUserBinding
+import com.example.myapplication.model.User
+import com.example.myapplication.model.UserToLogin
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: RegisterUserBinding;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = RegisterUserBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+    }
+
+
+    fun registerUser(view: View) {
+        GlobalScope.launch {
+            try {
+                var allUsers: List<User> = MainActivity.API.users.listAll();
+                var userToRegister: UserToLogin = UserToLogin(
+                    binding.txtUserNameRegister.text.toString(),
+                    binding.txtPasswordRegister.text.toString()
+                )
+                var duplicateUserFound = false;
+                for (user: User in allUsers) {
+                    if (user.username == userToRegister.username) {
+                        duplicateUserFound = true;
+                    }
+                }
+                if (!duplicateUserFound) {
+                    MainActivity.API.users.create(userToRegister)
+                    runOnUiThread {
+                        Toast.makeText(
+                            applicationContext,
+                            "User ${userToRegister.username} registered",
+                            Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                } else {
+                    runOnUiThread {
+                        Toast.makeText(
+                            applicationContext,
+                            "User already exists",
+                            Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                }
+            } catch (ex: Throwable) {
+
+                Log.e("Register Error", ex.toString())
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/services/UserService.kt
+++ app/src/main/java/com/example/myapplication/services/UserService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.*
 interface UserService {
     @GET("users/")          suspend fun listAll(): List<User>
     @GET("users/{id}")      suspend fun get(@Path("id") id: Long): User
-    @POST("users")          suspend fun create(@Body u: User): User
+    @POST("users")          suspend fun create(@Body u: UserToLogin): User
     @PUT("users/{id}")      suspend fun update(@Path("id") id: Int, @Body u: User)
     @DELETE("users/{id}")   suspend fun delete(@Path("id") id: Int)
     @POST("users/login")    suspend fun login(@Body u: UserToLogin): Long
